c11786b88a9c1f6079733198f961f4514e18dd68
malongwei1 <408544651@qq.com>
2018-01-17 10:15:56 +0800

    initial commit

--- hello.go
+++ hello.go
@@ -1,6 +1,8 @@
 package main
 import "fmt"
 func main(){
-		fmt.Printf("Hello,world.\n")
+		fmt.Printf("Hello,Git.\n")
+		fmt.Printf("Hello,Git.\n")
+		fmt.Printf("Hello,Git.\n")
 		fmt.Printf("Hello,Git.\n")
 }
